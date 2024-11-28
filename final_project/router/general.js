831f32ff310fe103a8bb52d23b9447bb8deebb5f
Artem <artem.matseruk@gmail.com>
2024-11-28 15:22:18 +0200

    feat: completed Task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,14 +1,24 @@
 const express = require('express');
-let books = require("./booksdb.js");
+const books = require("./booksdb.js");
 const {simplifyString} = require('../utils/simplifyString');
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
+const isValid = require("./auth_users.js").isValid;
+const { users } = require("./auth_users.js");
 const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body
+  
+  if (username && password) {
+    if (isValid(username)) {
+      users.push({ username, password })
+      return res.status(200).json({ message: 'User successfully registered. Now you can login' });
+    } else {
+      return res.status(404).json({ message: 'User already exists!' });
+    }
+  } else {
+    return res.status(404).json({message: 'Unable to register user. Missing username or password.'});
+  }
 });
 
 // Get the book list available in the shop
