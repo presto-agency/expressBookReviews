f8c8995860085f791649b7f6b0fb3f9f3b9ec2db
Artem <artem.matseruk@gmail.com>
2024-11-28 14:55:52 +0200

    feat: completed Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -70,8 +70,20 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const { isbn } = req.params
+    
+    const book = books[isbn]
+    
+    if (book) {
+      return res.status(200).json(book.reviews)
+    } else {
+      return res.status(404).json({ error: `No books found with this ISBN '${isbn}'` })
+    }
+  } catch (error) {
+    console.log(`Error with review request: ${error}`)
+    return res.status(500).json({ error: error.message })
+  }
 });
 
 module.exports.general = public_users;
